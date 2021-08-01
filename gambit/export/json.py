e228828a7df0c812e76a317e582b28d7cd27003f
Jared Lumpe <mjlumpe@gmail.com>
2021-08-01 10:01:05 -0600

    Base class for JSON results exporters

--- gambit/export/json.py
+++ gambit/export/json.py
@@ -18,41 +18,60 @@ def _todict(obj, attrs):
 	return {a: getattr(obj, a) for a in attrs}
 
 
+def asdict_method(recurse=False, **kw):
+	"""Create a ``to_json`` method which calls :func:`attrs.asdict` with the given options."""
+	def method(self, obj):
+		return asdict(obj, recurse=recurse, **kw)
+	return method
+
+
+asdict_default = asdict_method()
+
+
 @attrs()
-class JSONResultsExporter(AbstractResultsExporter):
-	"""Exports query results in JSON format.
+class BaseJSONResultsExporter(AbstractResultsExporter):
+	"""Base class for JSON exporters.
+
+	Subclasses need to implement the ``to_json`` method.
 
 	Attributes
 	----------
-	dense
-		Write with no whitespace to cut down on file size. Disable to produce more human-friendly
-		output. Defaults to True.
+	pretty
+		Write in more human-readable but less compact format. Defaults to False.
 	"""
-	dense: bool = attrib(default=True)
+	pretty: bool = attrib(default=False)
 
-	@singledispatchmethod
-	def _to_json(self, obj):
-		# Base case
+	def to_json(self, obj):
+		"""Convert object to JSON-compatible format (need not work recursively)."""
 		return gjson.to_json(obj)
 
-	_to_json.register(ClassifierResult, lambda self, result: asdict(result))
-	_to_json.register(QueryResults, lambda self, results: asdict(results))
-	_to_json.register(QueryResultItem, lambda self, item: asdict(item))
+	def export(self, file_or_path: Union[FilePath, IO], results: QueryResults):
+		opts = dict(indent=4, sort_keys=True) if self.pretty else dict()
+		with maybe_open(file_or_path, 'w') as f:
+			json.dump(results, f, default=self.to_json, **opts)
 
-	@_to_json.register(ReferenceGenomeSet)
+
+@attrs()
+class JSONResultsExporter(BaseJSONResultsExporter):
+	"""Exports query results in JSON format.
+	"""
+
+	to_json = singledispatchmethod(BaseJSONResultsExporter.to_json)
+
+	to_json.register(ClassifierResult, asdict_default)
+	to_json.register(QueryResults, asdict_default)
+	to_json.register(QueryResultItem, asdict_default)
+
+	@to_json.register(ReferenceGenomeSet)
 	def _genomeset_to_json(self, gset: ReferenceGenomeSet):
 		return _todict(gset, ['id', 'key', 'version', 'name', 'description'])
 
-	@_to_json.register(Taxon)
+	@to_json.register(Taxon)
 	def _taxon_to_json(self, taxon: Taxon):
 		return _todict(taxon, ['id', 'key', 'name', 'ncbi_id', 'distance_threshold'])
 
-	@_to_json.register(AnnotatedGenome)
+	@to_json.register(AnnotatedGenome)
 	def _genome_to_json(self, genome: AnnotatedGenome):
 		data = _todict(genome, ['key', 'description', 'organism', 'taxon', 'ncbi_db', 'ncbi_id', 'genbank_acc', 'refseq_acc'])
 		data['id'] = genome.genome_id
 		return data
-
-	def export(self, file_or_path: Union[FilePath, IO], results: QueryResults):
-		with maybe_open(file_or_path, 'w') as f:
-			json.dump(results, f, default=self._to_json)
