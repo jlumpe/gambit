aaf93f920d60dfdb37dffadc8962c90f691efeb7
Jared Lumpe <mjlumpe@gmail.com>
2021-08-02 10:55:54 -0600

    CSV results exporter

---
+++ gambit/export/csv.py
@@ -0,0 +1,65 @@
+"""Export query results to CSV."""
+
+import csv
+from typing import Dict, Any, List, Union, Iterable, TextIO
+
+from .base import AbstractResultsExporter
+from gambit.query import QueryResultItem, QueryResults
+from gambit.io.util import FilePath, maybe_open
+
+
+def getattr_nested(obj, attrs: Union[str, Iterable[str]], pass_none=False):
+	if isinstance(attrs, str):
+		attrs = attrs.split('.')
+
+	for attr in attrs:
+		if pass_none and obj is None:
+			return None
+
+		obj = getattr(obj, attr)
+
+	return obj
+
+
+class CSVResultsExporter(AbstractResultsExporter):
+	"""Exports query results in CSV format.
+
+	Attributes
+	----------
+	format_opts
+		Dialect and other formatting arguments passed to :func:`csv.write`\\ .
+	"""
+	format_opts: Dict[str, Any]
+
+	COLUMNS = [
+		('query.name', 'input.label'),
+		('query.path', 'input.file.path'),
+		('predicted.name', 'report_taxon.name'),
+		('predicted.rank', 'report_taxon.rank'),
+		('predicted.ncbi_id', 'report_taxon.ncbi_id'),
+		('predicted.threshold', 'report_taxon.distance_threshold'),
+		('closest.distance', 'classifier_result.closest_match.distance'),
+		('closest.description', 'classifier_result.closest_match.genome.description'),
+	]
+
+	def __init__(self, **format_opts):
+		if 'dialect' not in format_opts:
+			format_opts.setdefault('lineterminator', '\n')
+			format_opts.setdefault('quoting', csv.QUOTE_MINIMAL)
+		self.format_opts = format_opts
+
+	def get_header(self) -> List[str]:
+		"""Get values for header row."""
+		return [name for name, _ in self.COLUMNS]
+
+	def get_row(self, item: QueryResultItem) -> List:
+		"""Get row values for single result item."""
+		return [getattr_nested(item, attrs, pass_none=True) for _, attrs in self.COLUMNS]
+
+	def export(self, file_or_path: Union[FilePath, TextIO], results: QueryResults):
+		with maybe_open(file_or_path, 'w') as f:
+			writer = csv.writer(f, **self.format_opts)
+
+			writer.writerow(self.get_header())
+			for item in results.items:
+				writer.writerow(self.get_row(item))
--- tests/test_export.py
+++ tests/test_export.py
@@ -1,12 +1,15 @@
 from io import StringIO
+from csv import DictReader
 
 import pytest
+import numpy as np
 
 from gambit.query import QueryResults, QueryResultItem, QueryInput, QueryParams
 from gambit.classify import ClassifierResult, GenomeMatch
 from gambit.db import ReferenceGenomeSet, Genome
 from gambit.signatures import SignaturesMeta
 from gambit.io.seq import SequenceFile
+from gambit.export.csv import CSVResultsExporter
 from gambit.export.archive import ResultsArchiveReader, ResultsArchiveWriter
 
 
@@ -27,11 +30,15 @@ def results(session):
 	# Make classifier results
 	classifier_results = []
 	for i, taxon in enumerate(taxa):
+		# Pretend some of these are in the NCBI database
+		if i % 2:
+			taxon.ncbi_id = 1000 + i
+
 		genome = taxon.genomes.first()
 		assert genome is not None
 
 		match = GenomeMatch(
-			genome=taxon.genomes.first(),
+			genome=genome,
 			distance=(i + 1) / 100,
 			matched_taxon=taxon,
 		)
@@ -62,10 +69,11 @@ def results(session):
 	# Make result items
 	items = []
 	for i, cr in enumerate(classifier_results):
+		predicted = cr.predicted_taxon
 		items.append(QueryResultItem(
 			input=QueryInput(f'query-{i}', SequenceFile(f'query-{i}.fasta', 'fasta')),
 			classifier_result=cr,
-			report_taxon=cr.predicted_taxon,
+			report_taxon=None if predicted is None else predicted.parent if i % 4 == 0 else predicted,
 		))
 
 	return QueryResults(
@@ -80,6 +88,36 @@ def results(session):
 		),
 		extra=dict(foo=1),
 	)
+
+
+def test_csv(results):
+	"""Test CSVResultsExporter."""
+
+	buf = StringIO()
+	exporter = CSVResultsExporter()
+	exporter.export(buf, results)
+
+	buf.seek(0)
+	rows = list(DictReader(buf, **exporter.format_opts))
+	assert len(rows) == len(results.items)
+
+	for item, row in zip(results.items, rows):
+		assert row['query.name'] == item.input.label
+		assert row['query.path'] == str(item.input.file.path)
+
+		if item.report_taxon is None:
+			assert row['predicted.name'] == ''
+			assert row['predicted.rank'] == ''
+			assert row['predicted.ncbi_id'] == ''
+			assert row['predicted.threshold'] == ''
+		else:
+			assert row['predicted.name'] == item.report_taxon.name
+			assert row['predicted.rank'] == item.report_taxon.rank
+			assert row['predicted.ncbi_id'] == str(item.report_taxon.ncbi_id or '')
+			assert np.isclose(float(row['predicted.threshold']), item.report_taxon.distance_threshold)
+
+		assert np.isclose(float(row['closest.distance']), item.classifier_result.closest_match.distance)
+		assert row['closest.description'] == item.classifier_result.closest_match.genome.description
 
 
 def test_results_archive(session, results):
