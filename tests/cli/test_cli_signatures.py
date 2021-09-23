bc7adac6698e924542e97cc80439b8bc77758cc4
Jared Lumpe <mjlumpe@gmail.com>
2021-09-23 14:13:21 -0600

    Refactor 'signatures info' cmd tests

--- tests/cli/test_cli_signatures.py
+++ tests/cli/test_cli_signatures.py
@@ -2,6 +2,7 @@
 
 import json
 
+import pytest
 import numpy as np
 
 from gambit.cli.test import invoke_cli
@@ -9,13 +10,19 @@ import gambit.io.json as gjson
 
 
 class TestInfoCommand:
-	def test_standard(self):
-		pass  # TODO
 
-	def test_json(self, testdb_signatures, testdb_files):
-		args = ['signatures', 'info', str(testdb_files['ref_signatures']), '-j']
+	@pytest.fixture()
+	def base_args(self, testdb_files):
+		return ['signatures', 'info', str(testdb_files['ref_signatures'])]
 
-		result = invoke_cli(args)
+	def test_standard(self, base_args, testdb_signatures):
+		result = invoke_cli(base_args)
+		assert result.exit_code == 0
+
+		# TODO: check
+
+	def test_json(self, base_args, testdb_signatures):
+		result = invoke_cli([*base_args, '-j'])
 		assert result.exit_code == 0
 
 		data = json.loads(result.stdout)
@@ -23,10 +30,8 @@ class TestInfoCommand:
 		assert data['kmerspec'] == gjson.to_json(testdb_signatures.kmerspec)
 		assert data['metadata'] == gjson.to_json(testdb_signatures.meta)
 
-	def test_ids(self, testdb_signatures, testdb_files):
-		args = ['signatures', 'info', str(testdb_files['ref_signatures']), '-i']
-
-		result = invoke_cli(args)
+	def test_ids(self, base_args, testdb_signatures):
+		result = invoke_cli([*base_args, '-i'])
 		assert result.exit_code == 0
 
 		assert np.array_equal(result.stdout.splitlines(), testdb_signatures.ids)
