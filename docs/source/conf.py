52b0f77d733a5e7208c8e6ac36b96fa78fa78678
Jared Lumpe <mjlumpe@gmail.com>
2023-04-28 18:16:55 -0600

    Correct project version in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Jared Lumpe'
 author = 'Jared Lumpe'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
