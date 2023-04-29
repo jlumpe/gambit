6247e5a3df275f14201e54777816a5cfff32edf1
Jared Lumpe <mjlumpe@gmail.com>
2023-04-29 02:12:08 -0600

    Custom CSS in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,6 +63,8 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = ['css/custom.css']
+
 
 # -- Extension options -------------------------------------------------------
 
