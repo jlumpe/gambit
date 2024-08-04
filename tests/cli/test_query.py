181e4897b139ef340a3b04d59c5f27d13d6dfa04
Jared Lumpe <jared@jaredlumpe.com>
2024-08-03 17:31:01 -0700

    Clean up test code for CLI query

--- tests/cli/test_query.py
+++ tests/cli/test_query.py
@@ -2,96 +2,69 @@
 Test the 'gambit query' CLI command using the testdb_210818 database.
 """
 
-import os
 from copy import copy
+from typing import Optional, Iterable
+from pathlib import Path
 
 import pytest
 
 from gambit.cli.test import invoke_cli
 from gambit.results.test import check_json_results, check_csv_results
 from gambit.seq import SequenceFile
-from gambit.query import QueryInput
+from gambit.query import QueryInput, QueryResults
 from gambit.util.misc import zip_strict
-from gambit.util.io import write_lines
+from gambit.util.io import write_lines, FilePath
 from gambit.cli.common import strip_seq_file_ext
 
-
-@pytest.fixture(params=[None])
-def nqueries(request):
-	"""Number of testdb query files to use, None means use all of them.
-
-	Can be changed via indirect parameterization in specific tests.
-	Note than with slice notation, `[:None]` is the same as `[:]`.
-	"""
-	return request.param
+from ..testdb import TestDB
 
 
-@pytest.fixture()
-def query_files(testdb, nqueries):
-	"""Paths to query files."""
-	return [SequenceFile(f.path, f.format, f.compression) for f in testdb.get_query_files()[:nqueries]]
+def make_args(testdb: TestDB, *,
+			  positional_files: Optional[Iterable[SequenceFile]] = None,
+			  list_file: Optional[FilePath] = None,
+			  sig_file: bool = False,
+			  output: Optional[FilePath] = None,
+			  outfmt: Optional[str] = None,
+			  strict: bool=False,
+			  ) -> list[str]:
+	"""Make command line arguments for querying."""
+
+	args: list[str] = [f'--db={testdb.paths.root}', 'query']
+	args.append('--strict' if strict else '--no-strict')
+
+	if output is not None:
+		args.append(f'--output={output}')
+
+	if outfmt is not None:
+		args.append(f'--outfmt={outfmt}')
+
+	if positional_files is not None:
+		args.extend(map(str, positional_files))
+
+	if list_file is not None:
+		args += ['-l', str(list_file), f'--ldir={testdb.paths.query_genomes_dir}']
+
+	if sig_file:
+		args.append(f'--sigfile={testdb.paths.query_signatures}')
+
+	return args
 
 
-@pytest.fixture()
-def cd_query_genomes(testdb):
-	"""Change working directory to query genomes directory."""
-	old_wd = os.getcwd()
-	try:
-		os.chdir(testdb.paths.query_genomes_dir)
-		yield
-	finally:
-		os.chdir(old_wd)
-
-
-@pytest.fixture(name='make_args')
-def make_args_factory(testdb, query_files, tmp_path):
-
-	def make_args(positional=False, list_file=False, sig_file=False, output=None, outfmt=None, strict=False):
-		"""Make command line arguments for query file."""
-
-		args = [f'--db={testdb.paths.root}', 'query']
-		args.append('--strict' if strict else '--no-strict')
-
-		if output is not None:
-			args.append(f'--output={output}')
-
-		if outfmt is not None:
-			args.append(f'--outfmt={outfmt}')
-
-		if positional:
-			args.extend(query_files)
-
-		if list_file:
-			list_file = tmp_path / 'genomes.txt'
-			write_lines(query_files, list_file)
-			args += ['-l', str(list_file), f'--ldir={testdb.paths.query_genomes_dir}']
-
-		if sig_file:
-			args.append(f'--sigfile={testdb.paths.query_signatures}')
-
-		return list(map(str, args))
-
-	return make_args
-
-@pytest.fixture(name='make_ref_results')
-def make_ref_results_factory(testdb, nqueries, query_files):
+def make_ref_results(testdb: TestDB, inputs: Iterable[QueryInput], strict: bool, nqueries: Optional[int]):
 	"""
 	Make a copy of the reference query results to compare to, modifying to account for possibly
 	different query inputs and # of queries.
 	"""
-	def make_ref_results(strict, inputs):
-		ref_results = copy(testdb.get_query_results(strict))
-		ref_results.items = ref_results.items[:nqueries]
+	ref_results = copy(testdb.get_query_results(strict))
+	ref_results.items = ref_results.items[:nqueries]
 
-		for item, input in zip_strict(ref_results.items, inputs):
-			item.input = input
+	for item, input in zip_strict(ref_results.items, inputs):
+		item.input = input
 
-		return ref_results
-
-	return make_ref_results
+	return ref_results
 
 
-def check_results(results_file, out_fmt, ref_results):
+def check_results(results_file: Path, out_fmt: str, ref_results: QueryResults):
 	"""Check results output matches reference QueryResults object."""
 	if out_fmt == 'json':
 		with open(results_file) as fh:
@@ -113,25 +86,39 @@ def check_results(results_file, out_fmt, ref_results):
 		(None, False, 'json', False, True),
 		(20,   True,  'json', False, False),
 	],
-	indirect=['nqueries'],
 )
-def test_full_query(make_args, make_ref_results, use_list_file, out_fmt, strict, gzipped, query_files, tmp_path):
+def test_full_query(testdb: TestDB,
+					nqueries: Optional[int],
+					use_list_file: bool,
+					out_fmt: str,
+					strict: bool,
+					gzipped: bool,
+					tmp_path: Path,
+					):
 	"""Run a full query using the command line interface."""
 
+	query_files = testdb.get_query_files(gzipped)[:nqueries]
 	inputs = [
 		QueryInput(strip_seq_file_ext(file.path.name), file)
 		for file in query_files
 	]
-	ref_results = make_ref_results(strict, inputs)
+	ref_results: QueryResults = make_ref_results(testdb, inputs, strict, nqueries)
 
 	results_file = tmp_path / ('results.' + out_fmt)
 
+	if use_list_file:
+		list_file = tmp_path / 'genomes.txt'
+		write_lines(query_files, list_file)
+		input_kw = dict(list_file=list_file)
+	else:
+		input_kw = dict(positional_files=query_files)
+
 	args = make_args(
-		positional=not use_list_file,
-		list_file=use_list_file,
+		testdb,
 		output=results_file,
 		outfmt=out_fmt,
 		strict=strict,
+		**input_kw,
 	)
 
 	invoke_cli(args)
@@ -141,15 +128,16 @@ def test_full_query(make_args, make_ref_results, use_list_file, out_fmt, strict,
 # Not really necessary to check all combinations of parameters.
 @pytest.mark.parametrize('out_fmt', ['json'])
 @pytest.mark.parametrize('strict', [False])
-def test_sigfile(make_args, make_ref_results, testdb, out_fmt, strict, tmp_path):
+def test_sigfile(testdb: TestDB, out_fmt: str, strict: bool, tmp_path: Path):
 	"""Test using signature file instead of parsing genome files."""
 
 	inputs = list(map(QueryInput, testdb.query_signatures.ids))
-	ref_results = make_ref_results(strict, inputs)
+	ref_results = make_ref_results(testdb, inputs, strict, None)
 
 	results_file = tmp_path / ('results.' + out_fmt)
 
 	args = make_args(
+		testdb,
 		sig_file=True,
 		output=results_file,
 		outfmt=out_fmt,
@@ -160,19 +148,27 @@ def test_sigfile(make_args, make_ref_results, testdb, out_fmt, strict, tmp_path)
 	check_results(results_file, out_fmt, ref_results)
 
 
-def test_invalid(make_args, tmp_path):
+def test_invalid(testdb: TestDB, tmp_path: Path):
 	"""Test invalid parameter values exit with error code."""
 
+	query_files = testdb.get_query_files()
+	list_file = tmp_path / 'list.json'
+	write_lines(query_files, list_file)
 	results_file = tmp_path / ('results.json')
 
 	# No genomes or signatures
-	args = make_args(output=results_file)
-	invoke_cli(args, success=False)
+	args = make_args(testdb, output=results_file)
+	result = invoke_cli(args, success=False)
+	assert result.stderr.strip() == 'Error: One of GENOMES, -l, or -s/--sigfile is required'
 
 	# Multiple inputs
-	args = make_args(output=results_file, positional=True, list_file=True)
-	assert invoke_cli(args, success=False)
-	args = make_args(output=results_file, positional=True, sig_file=True)
-	assert invoke_cli(args, success=False)
-	args = make_args(output=results_file, list_file=True, sig_file=True)
-	assert invoke_cli(args, success=False)
+	multi_msg = 'Error: GENOMES, -l, and -s/--sigfile are mutually exclusive'
+
+	args = make_args(testdb, output=results_file, positional_files=query_files, list_file=list_file)
+	assert invoke_cli(args, success=False).stderr.strip() == multi_msg
+
+	args = make_args(testdb, output=results_file, positional_files=query_files, sig_file=True)
+	assert invoke_cli(args, success=False).stderr.strip() == multi_msg
+
+	args = make_args(testdb, output=results_file, list_file=list_file, sig_file=True)
+	assert invoke_cli(args, success=False).stderr.strip() == multi_msg
